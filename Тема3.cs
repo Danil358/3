35060f273e600079569600ab023c20367e8099ad
Danil358 <kovalenkodanil931@gmail.com>
2024-11-23 09:04:53 +0300

    Create Тема3.cs

---
+++ Тема3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tema3;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hot func = new Hot();
+
+            func.HelloWorld("Данил");
+            func.Surname("Коваленко");
+            Console.ReadLine();
+
+
+
+            Calculator calc = new Calculator();
+            Console.WriteLine($"3 + 3 = {calc.Add(3, 3)}");
+            Console.WriteLine($"3 - 3 = {calc.Subtract(3, 3)}");
+            Console.WriteLine($"3 * 3 = {calc.Multiply(3, 3)}");
+        }
+        
+            
+        
+    }
+}
